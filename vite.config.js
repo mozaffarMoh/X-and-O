a112fe7c350625be7573298e5ee7a1714b460843
mozaffarMoh <ferawwwrss@gmail.com>
2025-07-20 21:00:56 +0300

    pwa2

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,43 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.png"],
       manifest: {
-        name: 'My React Vite App',
-        short_name: 'ReactApp',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#1976d2',
+        name: "X and O game",
+        short_name: "XOGAME",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#1976d2",
+        screenshots: [
+          {
+            src: "favicon.png",
+            type: "image/png",
+            sizes: "1280x720",
+            form_factor: "wide", // or "narrow" for mobile
+          },
+        ],
         icons: [
           {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
+            src: "favicon.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
           },
           {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
-  ]
+            src: "favicon.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+        ],
+      },
+    }),
+  ],
 });
